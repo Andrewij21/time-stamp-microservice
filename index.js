8f176d2573a565e22e98af4f844c35b4f79867a6
Andrewij21 <andrewijaya988@gmail.com>
2023-01-21 18:26:57 +0700

    Fix port

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ app.get("/api/:date", (req, res) => {
 });
 
 // listen for requests :)
-// var listener = app.listen(process.env.PORT, function () {
-//   console.log("Your app is listening on port " + listener.address().port);
-// });
-var listener = app.listen(52185, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+// var listener = app.listen(52185, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
